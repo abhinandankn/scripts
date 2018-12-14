52f6c8ede7d3daf7a254002e8f8f527e0e560d28
Abhinandan K N <abhikn@outlook.com>
2018-12-14 15:52:36 +0530

    Moved post installation scripts to dir post_install

---
+++ post_install/README.md
@@ -0,0 +1,21 @@
+### Post Install Scripts
+
+Scripts to run after fresh installaion of Ubuntu. The scipt does the following
+1. Install required packages.(`install_packages.sh`)
+2. Install necessary python packages, set up bash, vim, spacemacs .etc. (`setup.sh`)
+3. Suggest a list of customizations to perform after installation. (`post_install.sh`)
+
+#### Installation
+
+Clone this repository using the following
+(`git clone https://peregrine_falcon@bitbucket.org/peregrine_falcon/scripts.git ~/Downloads/scripts`)
+
+Change directory to cloned repository
+(`cd ~/Downloads/scripts/post_install`)
+
+Give permissions
+(`chmod +x install.sh`)
+
+Runi (Note: Requires eleveted permissions)
+(`./install.sh`)
+
---
+++ post_install/install_packages.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# Title: install_packages.sh
+# Author: Abhinandan K N
+
+echo "Running install_packages.sh"
+echo "To be run with eleveted previlages"
+
+# Initialize
+cd ~                         # Change Directory to Home
+apt update && apt upgrade -y # Initial Update and Upgrade
+clear
+
+# Install Basic Packages
+apt install -y \
+    build-essential \
+    clang \
+    curl \
+    emacs \
+    fonts-powerline \
+    gcc \
+    git \
+    gnome-tweaks \
+    htop \
+    python3-dev \
+    python3-pip \
+    synaptic \
+    tmux \
+    vim \
+    virtualbox \
+    vlc \
+    zathura
+echo "Finished Installing Basic Packages"
+
+# Install additional packages
+apt-add-repository -y ppa:teejee2008/ppa                                       # Timeshift
+apt-add-repository -y ppa:system76/pop                                         # Pop Theme
+apt-add-repository -y ppa:danielrichter2007/grub-customizer                    # grub-customizer
+apt update && apt upgrade -y                                                   # update and upgrade after ppa
+
+apt install -y \
+    timeshift \
+    pop-theme \
+    grub-customizer
+
+cd ~/Downloads
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb # Google Chrome
+apt install -y ~/Downloads/google-chrome-stable_current_amd64.deb 
+clear
+echo "Finished Installing Additional Packages"
+
+# Update and upgrade
+apt update && apt upgrade -y
+clear
+echo "Finished Updating and Upgrading"
+
+# Clean up
+apt autoremove -y
+rm ~/Downloads/google-chrome-stable_current_amd64.deb
+clear
+
